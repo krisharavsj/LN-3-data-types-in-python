30ce55b7259e4648e86e0410646452f97a68788b
krisharavsj <krisharsv@gmail.com>
2025-05-22 09:39:07 +0530

    type casting.py

---
+++ type casting.py
@@ -0,0 +1,16 @@
+x = True 
+c = "number"
+m = 17
+q = 17.25
+print ("The values before type casting are")
+print (x,type(x))
+print(c,type(c))
+print(m,type(m))
+print(q,type(q)) 
+print("The values after type casting are")
+q = int (q)
+print (q,type(q))
+m = float (m)
+print(m,type(m))
+x = str (x)
+print (x,type(x))
